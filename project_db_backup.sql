17de630248b0c99f3b351b7756068e7d06778d44
ManaRegen <132016620+ManaRegen@users.noreply.github.com>
2024-11-08 23:04:24 -0800

    chore: initialize database tables

--- project_db_backup.sql
+++ project_db_backup.sql
@@ -1,31 +1,150 @@
-
-CREATE DATABASE IF NOT EXISTS project_db;
-USE project_db;
-/*M!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19-11.5.2-MariaDB, for osx10.19 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: project_db
 -- ------------------------------------------------------
--- Server version	9.0.1
+-- Server version	8.0.36
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `User`
+-- Table structure for table `equipment`
 --
-DROP TABLE IF EXISTS `User`;
+
+DROP TABLE IF EXISTS `equipment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `User` (
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `equipment` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(99) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipment`
+--
+
+LOCK TABLES `equipment` WRITE;
+/*!40000 ALTER TABLE `equipment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exercise_equipment`
+--
+
+DROP TABLE IF EXISTS `exercise_equipment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exercise_equipment` (
+  `exercise_id` int NOT NULL,
+  `equipment_id` int NOT NULL,
+  `quantity` int DEFAULT NULL,
+  PRIMARY KEY (`exercise_id`,`equipment_id`),
+  KEY `equipment_id` (`equipment_id`),
+  CONSTRAINT `exercise_equipment_ibfk_1` FOREIGN KEY (`exercise_id`) REFERENCES `gym_exercise` (`id`),
+  CONSTRAINT `exercise_equipment_ibfk_2` FOREIGN KEY (`equipment_id`) REFERENCES `equipment` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exercise_equipment`
+--
+
+LOCK TABLES `exercise_equipment` WRITE;
+/*!40000 ALTER TABLE `exercise_equipment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exercise_equipment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exercise_muscle_groups`
+--
+
+DROP TABLE IF EXISTS `exercise_muscle_groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exercise_muscle_groups` (
+  `exercise_id` int NOT NULL,
+  `muscle_id` int NOT NULL,
+  PRIMARY KEY (`exercise_id`,`muscle_id`),
+  KEY `muscle_id` (`muscle_id`),
+  CONSTRAINT `exercise_muscle_groups_ibfk_1` FOREIGN KEY (`exercise_id`) REFERENCES `gym_exercise` (`id`),
+  CONSTRAINT `exercise_muscle_groups_ibfk_2` FOREIGN KEY (`muscle_id`) REFERENCES `muscle_groups` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exercise_muscle_groups`
+--
+
+LOCK TABLES `exercise_muscle_groups` WRITE;
+/*!40000 ALTER TABLE `exercise_muscle_groups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exercise_muscle_groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gym_exercise`
+--
+
+DROP TABLE IF EXISTS `gym_exercise`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gym_exercise` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(99) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gym_exercise`
+--
+
+LOCK TABLES `gym_exercise` WRITE;
+/*!40000 ALTER TABLE `gym_exercise` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gym_exercise` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `muscle_groups`
+--
+
+DROP TABLE IF EXISTS `muscle_groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `muscle_groups` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(99) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `muscle_groups`
+--
+
+LOCK TABLES `muscle_groups` WRITE;
+/*!40000 ALTER TABLE `muscle_groups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `muscle_groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(99) NOT NULL,
   `middle_initial` char(1) DEFAULT NULL,
@@ -37,12 +156,100 @@ CREATE TABLE `User` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `User`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `User` WRITE;
-/*!40000 ALTER TABLE `User` DISABLE KEYS */;
-/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `workout_plan`
+--
+
+DROP TABLE IF EXISTS `workout_plan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `workout_plan` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(99) DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `workout_plan_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `workout_plan`
+--
+
+LOCK TABLES `workout_plan` WRITE;
+/*!40000 ALTER TABLE `workout_plan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `workout_plan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `workout_plan_set`
+--
+
+DROP TABLE IF EXISTS `workout_plan_set`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `workout_plan_set` (
+  `plan_id` int NOT NULL,
+  `exercise_id` int NOT NULL,
+  `set_type` enum('normal','warmup') DEFAULT NULL,
+  `rep_count` int DEFAULT NULL,
+  `order_no` int DEFAULT NULL,
+  `weight_lbs` decimal(2,1) DEFAULT NULL,
+  PRIMARY KEY (`plan_id`,`exercise_id`),
+  KEY `exercise_id` (`exercise_id`),
+  CONSTRAINT `workout_plan_set_ibfk_1` FOREIGN KEY (`plan_id`) REFERENCES `workout_plan` (`id`),
+  CONSTRAINT `workout_plan_set_ibfk_2` FOREIGN KEY (`exercise_id`) REFERENCES `gym_exercise` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `workout_plan_set`
+--
+
+LOCK TABLES `workout_plan_set` WRITE;
+/*!40000 ALTER TABLE `workout_plan_set` DISABLE KEYS */;
+/*!40000 ALTER TABLE `workout_plan_set` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `workout_session`
+--
+
+DROP TABLE IF EXISTS `workout_session`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `workout_session` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `plan_id` int DEFAULT NULL,
+  `name` varchar(99) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `duration_minutes` int DEFAULT NULL,
+  `location` varchar(99) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `plan_id` (`plan_id`),
+  CONSTRAINT `workout_session_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`Id`),
+  CONSTRAINT `workout_session_ibfk_2` FOREIGN KEY (`plan_id`) REFERENCES `workout_plan` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `workout_session`
+--
+
+LOCK TABLES `workout_session` WRITE;
+/*!40000 ALTER TABLE `workout_session` DISABLE KEYS */;
+/*!40000 ALTER TABLE `workout_session` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -52,6 +259,6 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-03 16:13:05
+-- Dump completed on 2024-11-08 23:03:58
